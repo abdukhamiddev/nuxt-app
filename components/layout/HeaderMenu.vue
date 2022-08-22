5fae977b0321601187c5c323550f9ac77199f4cd
abdukhamid <latipovabdukhamid@gmail.com>
2022-08-22 13:09:04 +0500

    navbar and scroll directive

--- components/layout/HeaderMenu.vue
+++ components/layout/HeaderMenu.vue
@@ -1,17 +1,103 @@
 <template>
-    <div>
-        <h1>Header</h1>
+    <div class="fixed z-[11] bg-transparent w-full" ref="menuHeaderRef" v-scroll="handleScroll">
+        <nav class="relative px-0 h-[110px] flex pt-2 px4 justify-between items-center flex-wrap">
+            <div class="flex items-center justify-between w-full px-4 lg:px-12">
+
+                <button ref="toggler" @click="toggleHandler" class="bg-transparent cursor-pointer
+                    border-[hsla(0,0%,100%,.1)] rounded-md border border-solid py-1 px-3
+                    focus:outline-black focus:outline-[2px] focus:outline">
+                    <HamburgerMenu class="w-5 h-5 transition-all duration-300 black-ico">
+                    </HamburgerMenu>
+                </button>
+
+
+
+                <div class="flex justify-center absolute left-[50%] translate-x-[-50%]">
+                    <nuxt-link to="/">
+                        <img src="~/assets/img/ico/lyonn-positive.svg" alt="Agency Logo" class="w-8 h-8" />
+                    </nuxt-link>
+                </div>
+                <div class="flex items-center cursor-pointer text-dark underline-hover" href="#">
+                    <span class="font-gilroy font-700 text-[12px] ">Contact Us</span>
+                    <img class="w-4 h-4 mb-1 ml-4 align-middle" src="~/assets/img/ico/wp-positive.svg"
+                        alt="Whatsapp Lyonn Contact" />
+                </div>
+            </div>
+            <div class="w-[100vw] fixed  bg-[#000000]  z-20  min-h-screen  inset-0 translate-y-[-100%] transition-transform duration-300"
+                :class="showMenu ? 'translate-y-[]' : ''">
+                <button ref="toggler" @click="toggleHandler"
+                    class="z-30 bg-transparent cursor-pointer border-[hsla(0,0%,100%,.1)] rounded-md border border-solid py-1 px-3"
+                    :class="toggled ? 'focus:outline-[2px] focus:outline  focus:outline-slate-100' : 'focus:outline-black focus:outline-[1px] focus:outline'">
+                    <HamburgerMenu class="w-5 h-5 transition-all duration-300 black-ico"
+                        :class="toggled ? 'white-ico' : 'black-ico'" :x22="toggled ? '21' : '22'">
+                    </HamburgerMenu>
+                </button>
+            </div>
+        </nav>
+
     </div>
 </template>
 
 <script>
+import HamburgerMenu from '../icons/HamburgerMenu.vue';
+
+
 export default {
+
+
     setup() {
 
+    },
+    components: { HamburgerMenu },
+    data() {
+        return {
+            headerHeight: 80,
+            showMenu: false,
+            headerElement: null,
+            toggled: false
+        };
+    },
+    directives: {
+        scroll: {
+            mounted(el, binding) {
+                let f = function (evt) {
+                    if (binding.value(evt, el)) {
+                        window.removeEventListener('scroll', f)
+                    }
+                    console.log(el);
+                }
+                window.addEventListener('scroll', f)
+            }
 
-        return {}
-    }
+        }
+    },
+    mounted() {
+        this.headerElement = this.$refs.menuHeaderRef
+    },
+    methods: {
+        handleScroll(evt, el) {
+            if (
+                window.scrollY > this.headerHeight &&
+                !this.headerElement.classList.contains('menu-white')
+            ) {
+                this.headerElement.classList.add('menu-white')
+            } else if (
+                window.scrollY < this.headerHeight &&
+                this.headerElement.classList.contains('menu-white')
+            ) {
+                this.headerElement.classList.remove('menu-white')
+            }
+        },
+        toggleHandler(evt) {
+            this.showMenu = !this.showMenu;
+            this.toggled = !this.toggled
+        }
+    },
+
+
 }
+
+
 </script>
 
 <style lang="scss" scoped>
--- pages/index.vue
+++ pages/index.vue
@@ -2,20 +2,22 @@
     <div class="overflow-hidden">
         <HeaderMenu></HeaderMenu>
         <section class="flex-wrap">
-            <div
-                class="h-[calc(100vh_-_25px)] relative flex overflow-hidden rm:h-[400px] md:h-full lg:h-[calc(100vh_-_20px)]">
+            <div class="relative flex overflow-hidden   lg:h-[calc(100vh_-_10px)] h-screen">
                 <BlueCircle
                     class="absolute w-[300px] h-[300px] z-[-1] bg-contain bg-no-repeat bg-center translate-y-[50%] translate-x-[70%] overflow-in right-0  md:w-[400px] md:h-[400px] md:translate-x-[70%] md:translate-y-[30%]  lg:w-[533px] lg:h-[533px] lg:translate-x-[50%] lg:translate-y-[20%]" />
                 <div class="container flex flex-wrap items-center justify-center h-full mx-auto">
                     <div class="flex items-center justify-center w-11/12 pt-20 text-center ">
                         <h1
-                            class="text-center font-gilroy lg:text-[78px] lg:leading-[93.5px] font-semibold md:text-[60px] text-[40px] ">
+                            class="text-center font-gilroy lg:text-[78px] lg:leading-[93.5px] font-[500] md:text-[60px] text-[35px] ">
                             We are Lyonn, Your Digital Agency
                         </h1>
                     </div>
                     <div class="flex items-center justify-center mt-2 pb-14">
-                        <button class="mx-5 my-2 btn-default btn-black sm:mx-2 md:mx-2">Start Now</button>
-                        <NuxtLink class="mx-5 my-2 btn-default btn-white sm:mx-2 md:mx-2 " href="#">View Portfolio
+                        <button class="mx-5 my-2 text-xs btn-default btn-black sm:mx-2 md:mx-2 md:text-sm">Start
+                            Now</button>
+                        <NuxtLink class="mx-5 my-2 text-xs btn-default btn-white sm:mx-2 md:mx-2 md:text-sm" href="#">
+                            View
+                            Portfolio
                         </NuxtLink>
                     </div>
 
@@ -23,9 +25,7 @@
                 <RedCircle class="absolute w-[300px] h-[300px] z-[-1] bg-contain bg-no-repeat bg-center translate-y-[40%]
                         translate-x-[-80%] md:w-[400px] md:h-[400px] md:translate-x-[-75%] md:translate-y-[15%]
                         lg:w-[533px] lg:h-[533px] lg:translate-x-[-50%] lg:translate-y-[15%]" />
-
             </div>
-
         </section>
     </div>
 </template>
