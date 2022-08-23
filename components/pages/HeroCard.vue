8bd5913939acf12e6d3fd62b94ada2712d2ea991
abdukhamid <latipovabdukhamid@gmail.com>
2022-08-23 11:52:46 +0500

    hero section

---
+++ components/pages/HeroCard.vue
@@ -0,0 +1,30 @@
+<template>
+    <div
+        class="cursor-pointer  h-60 rounded-[30px] transition-shadow duration-[0.3s] ease-linear hover:shadow-[0_0_80px_3px_rgba(204,204,204,.3)]">
+        <div class="flex flex-col items-center px-6 py-6 text-center">
+            <img class="w-12 h-12 " :src="imageUrl" alt="">
+            <h2 class="pr-6 mt-6 text-xl font-semibold font-gilroy">{{ text }}</h2>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        imageUrl: {
+            type: String
+        },
+        text: {
+            type: String
+        }
+    },
+    setup() {
+
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
     <div class="overflow-hidden">
         <HeaderMenu></HeaderMenu>
         <section class="flex-wrap">
-            <div class="relative flex overflow-hidden   lg:h-[calc(100vh_-_10px)] h-screen">
+            <div class="relative flex lg:h-[calc(100vh_-_10px)] h-screen">
                 <BlueCircle
                     class="absolute w-[300px] h-[300px] z-[-1] bg-contain bg-no-repeat bg-center translate-y-[50%] translate-x-[70%] overflow-in right-0  md:w-[400px] md:h-[400px] md:translate-x-[70%] md:translate-y-[30%]  lg:w-[533px] lg:h-[533px] lg:translate-x-[50%] lg:translate-y-[20%]" />
                 <div class="container flex flex-wrap items-center justify-center h-full mx-auto">
@@ -12,7 +12,7 @@
                             We are Lyonn, Your Digital Agency
                         </h1>
                     </div>
-                    <div class="flex items-center justify-center mt-2 pb-14">
+                    <div class="flex items-center justify-center mt-6 ">
                         <button class="mx-5 my-2 text-xs btn-default btn-black sm:mx-2 md:mx-2 md:text-sm">Start
                             Now</button>
                         <NuxtLink class="mx-5 my-2 text-xs btn-default btn-white sm:mx-2 md:mx-2 md:text-sm" href="#">
@@ -26,14 +26,41 @@
                         translate-x-[-80%] md:w-[400px] md:h-[400px] md:translate-x-[-75%] md:translate-y-[15%]
                         lg:w-[533px] lg:h-[533px] lg:translate-x-[-50%] lg:translate-y-[15%]" />
             </div>
+            <div
+                class="flex md:max-w-[85%] lg:max-w-[70%] items-center justify-center mx-auto max-w-[90%] flex-col md:flex-row mt-12">
+                <div>
+                    <HeroCard imageUrl="/assets/img/ico/area-design.svg" text="Graphic Design." />
+                </div>
+                <div>
+                    <HeroCard imageUrl="/assets/img/ico/area-d.t.svg" text="Web Development." />
+                </div>
+                <div>
+                    <HeroCard imageUrl="/assets/img/ico/area-m.k.svg" text="Digital Marketing." />
+                </div>
+                <div>
+                    <HeroCard imageUrl="/assets/img/ico/area-p.a.svg" text="Audiovisual production." />
+                </div>
+            </div>
+        </section>
+
+        <section>
+            <div class="flex items-center justify-between container-body">
+                <div class="">
+                    <h2 class="text-[40px] font-semibold text-center font-gilroy sm:text-left">Our
+                        <br>
+                        Services.
+                    </h2>
+                </div>
+            </div>
         </section>
     </div>
 </template>
 
 <script>
-import BlueCircle from '~~/components/icons/BlueCircle.vue'
+import BlueCircle from '~~/components/icons/BlueCircle'
 import RedCircle from '~~/components/icons/RedCircle'
 import HeaderMenu from '~~/components/layout/HeaderMenu'
+import HeroCard from '~~/components/pages/HeroCard'
 
 
 
@@ -42,7 +69,7 @@ export default {
         BlueCircle,
         RedCircle,
         HeaderMenu,
-
+        HeroCard
     },
     setup() {
 
